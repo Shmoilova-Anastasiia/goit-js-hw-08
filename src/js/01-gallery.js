89756c444c47fe66b5a72adf05a7983a62754fe1
Shmoilova-Anastasiia <114925791+Shmoilova-Anastasiia@users.noreply.github.com>
2023-03-11 15:56:21 +0300

    task01

---
+++ src/js/01-gallery.js
@@ -0,0 +1,25 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+const makeGallery = galleryItems.map((element) =>{
+
+const itemGalleryEl = `<a class="gallery__item" href="${element.original}">
+    <img
+        class="gallery__image"
+        src="${element.preview}"
+        alt="${element.description}"
+    />
+    </a>`
+    return itemGalleryEl
+}).join('')
+galleryEl.innerHTML = makeGallery;
+
